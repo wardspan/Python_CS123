87c53f0f2a370d88e36f9592ae41e32f4bf85f25
Ward Spangenberg <wardspan@mac.com>
2024-02-07 15:39:15 -0500

    missed one

--- age_classifier.py
+++ age_classifier.py
@@ -1,14 +1,12 @@
 print("Welcome to the age classifier!")
-print("This program will classify a person's age into one of the following categories: infant, child, teenager, adult, or senior.") 
+print("This program will classify a person's age into one of the following categories: infant, child, teenager, or adult.") 
 age = int(input("Enter the person's age: "))
-if age < 1:
+if age <= 1:
     print("The person is an infant.")
 elif age < 13:
     print("The person is a child.")
 elif age < 20:
     print("The person is a teenager.")
-elif age < 65:
-    print("The person is an adult.")
 else:
-    print("The person is a senior.")
+    print("The person is an adult.")
 print("Goodbye!")
