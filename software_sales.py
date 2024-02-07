2cf3d68f9be702c97f1a4f6aa9ea64afc6a493b3
Ward Spangenberg <wardspan@mac.com>
2024-02-07 15:49:55 -0500

    updated software purchase

--- software_sales.py
+++ software_sales.py
@@ -9,5 +9,11 @@ elif units >=50 and units < 99:
     discount = .30
 else:
     discount = .40
-print(f"The discount on your purchase is {discount:.0%}.")
-print("Goodbye!")  # display goodbye message    
+total_cost = units * 99
+total_cost_discount = units * 99 * (1 - discount)
+savings = total_cost - total_cost_discount
+print(f"You have earned a {discount * 100:.0f}% discount on your purchase.") # display the discount
+print(f"The total cost of your purchase before discount is ${total_cost:.2f}")  # display the total cost
+print(f"The total cost of your purchase after discount is ${total_cost_discount:.2f}")  # display the discount cost
+print(f"You saved ${savings:.2f} today!")  # display the savings cost
+print("Goodbye!")
